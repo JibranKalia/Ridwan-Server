a8e7ce803fdf664214b19660ad598f48e158009b
Jibran Kalia <jibran.kalia@gmail.com>
2019-11-25 00:53:30 -0600

    annotating

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   has_secure_password
 end
