ff0d9acd2182f49caab44d8d8ddacdd93f65d50f
Jibran Kalia <jibran.kalia@gmail.com>
2019-12-03 21:54:33 -0600

    Initial classroom controller

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -9,4 +9,28 @@ class ClassroomsController < ApplicationController
     authorize @classroom
     render json: @classroom
   end
+
+  def create
+    @classroom = Classroom.new(resource_params)
+    authorize @classroom
+    @classroom.save
+    render json: @classroom
+  end
+
+  def destroy
+    @classroom = Classroom.find(params[:id])
+    authorize @classroom
+    @classroom.destroy
+    render json: @classroom
+  end
+
+  private
+
+  def permitted_attributes
+    %i[name user]
+  end
+
+  def resource_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: permitted_attributes)
+  end
 end
