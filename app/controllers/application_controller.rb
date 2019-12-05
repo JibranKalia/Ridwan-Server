543b8c4ffab6c58de90e03318dff320ab77bd867
Jibran Kalia <jibran.kalia@gmail.com>
2019-12-05 12:49:08 -0600

    configuring signup paramters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+  end
 end
