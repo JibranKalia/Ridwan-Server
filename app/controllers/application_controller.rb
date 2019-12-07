0f8dbfe9d6c0847182e89db45eed913747bd66ab
Jibran Kalia <jibran.kalia@gmail.com>
2019-12-07 00:32:28 -0600

    Added exception handliung

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,17 @@ class ApplicationController < ActionController::API
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+  end
+
+  def user_not_authorized(exception)
+    self.status        = 401
+    self.content_type  = 'application/json'
+    self.response_body = { errors: [{ status: '401', detail: exception.message }]}.to_json
   end
 end
