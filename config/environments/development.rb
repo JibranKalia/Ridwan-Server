c3bcbb2a0569cb63fbed885c0f3f3073ccfddcd5
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-13 01:36:54 -0600

    AB#50 Password reset flow (#24)
    * Trying to get sendgrid working
    
    * development settings
    
    * setting values

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,23 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true 
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { :host => ENV['API_HOST']}
+  config.action_mailer.default_options = { from: 'support@ridwan.io' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => ENV['SMTP_DOMAIN'], 
+    :address => ENV['SMTP_ADDRESS'],
+    :port => ENV['SMTP_PORT'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { :host => ENV['API_HOST']}
+  config.action_mailer.default_options = { from: 'support@ridwan.io' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => ENV['SMTP_DOMAIN'], 
+    :address => ENV['SMTP_ADDRESS'],
+    :port => ENV['SMTP_PORT'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
