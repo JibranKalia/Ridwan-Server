9de5e90fef7bfa5d12b158da15f86db11bf9db24
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-13 00:11:44 -0600

    development settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,20 +34,21 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true 
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.default_url_options = { host: 'ridwan-server.ngrok.io'}
+  config.action_mailer.default_url_options = { :host => ENV['API_HOST']}
+  config.action_mailer.default_options = { from: 'jibran.kalia@gmail.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => ENV['SMTP_DOMAIN'], 
-    :address => "smtp.sendgrid.net",
-    :port => ENV['SMTP_PORT'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  #   :domain => ENV['SMTP_DOMAIN'], 
+  #   :address => "smtp.sendgrid.net",
+  #   :port => ENV['SMTP_PORT'],
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
