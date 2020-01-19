78f21dc18cd6d6bb9d0aaa8477a05218b18f8671
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-19 17:50:15 -0600

    Fixing role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name email password password_confirmation role])
   end
 
   def user_not_authorized(exception)
