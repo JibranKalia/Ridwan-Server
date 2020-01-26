7a1f1eae3178f40be7b95815a2a10198d0bc1b45
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-26 11:58:34 -0600

    Lesson types controller and seralizer

---
+++ app/controllers/lesson_types_controller.rb
@@ -0,0 +1,21 @@
+class LessonTypesController < ApplicationController
+  def update
+    @lesson_type_type = LessonType.find(params[:id])
+    authorize @lesson_type
+    if @lesson_type.update(resource_params)
+      render json: @lesson_type
+    else
+      render json: @lesson_type, status: :unprocessable_entity, adapter: :json_api, serializer: ActiveModel::Serializer::ErrorSerializer
+    end
+  end
+
+  private
+
+  def permitted_attributes
+    %i[name]
+  end
+
+  def resource_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: permitted_attributes)
+  end
+end
